d3c9c3559bb320f81ec8b712c4b4472524c2b55b
Anthony Dominguez <adominguez15722@gmail.com>
2022-04-18 15:29:45 -0500

    testing while loop

--- src/com/mortgageCalculator.java
+++ src/com/mortgageCalculator.java
@@ -1,21 +1,33 @@
 package com;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class mortgageCalculator {
     public static void main(String[] args) {
-        Scanner number = new Scanner(System.in);
+//        Scanner number = new Scanner(System.in);
+//
+//        System.out.println("Number: ");
+//        int input = number.nextInt();
+//        if (input % 5 == 0 && input % 3 == 0)
+//            System.out.println("Fizzbuzz");
+//        else if (input % 5 == 0)
+//            System.out.println("Fizz");
+//        else if (input % 3 == 0)
+//            System.out.println("Buzz");
+//        else
+//            System.out.println(input);
 
-        System.out.println("Number: ");
-        int input = number.nextInt();
-        if (input % 5 == 0 && input % 3 == 0)
-            System.out.println("Fizzbuzz");
-        else if (input % 5 == 0 && input % 3 > 0)
-            System.out.println("Fizz");
-        else if (input % 5 > 0 && input % 3 == 0)
-            System.out.println("Buzz");
-        else {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("quit"))
+                break;
             System.out.println(input);
         }
+
     }
 }
